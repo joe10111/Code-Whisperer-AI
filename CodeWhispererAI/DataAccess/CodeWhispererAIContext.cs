1a61f60d97fe5653012cf8fad3c9c54451642a28
Joe Centeno <centeno.joe@gmail.com>
2023-11-07 10:04:51 -0800

    Added DB Connection, Logging, and DB Tables / Models.

---
+++ CodeWhispererAI/DataAccess/CodeWhispererAIContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CodeWhispererAI.Models;
+
+
+namespace CodeWhispererAI.DataAccess
+{
+    public class CodeWhispererAIContext : DbContext
+    {
+        public DbSet<CodeSnippet> CodeSnippets { get; set; }
+        public DbSet<CodeAnalysis> CodeAnalyses { get; set; }
+        public CodeWhispererAIContext(DbContextOptions<CodeWhispererAIContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ CodeWhispererAI/Models/CodeSnippet.cs
@@ -0,0 +1,11 @@
+﻿namespace CodeWhispererAI.Models
+{
+    public class CodeSnippet
+    {
+        public int Id { get; set; }        
+        public string? CodeInputted { get; set; }
+        public string? LanguageUsed { get; set; }
+        public int ResponseStatus { get; set; }
+        public int FeedbackID { get; set; }
+    }
+}
