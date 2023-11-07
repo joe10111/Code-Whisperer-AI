1f34497be6d63e84807ddc585218c692ca9ca5c3
Joe Centeno <centeno.joe@gmail.com>
2023-11-07 14:15:15 -0800

    Implement the Foundation for Code Analysis Feature

---
+++ CodeWhispererAI/Models/ChatCompletion.cs
@@ -0,0 +1,33 @@
+﻿namespace CodeWhispererAI.Models
+{
+    public class ChatCompletion
+    {
+        public string Id { get; set; }
+        public string Object { get; set; }
+        public long Created { get; set; }
+        public string Model { get; set; }
+        public string SystemFingerprint { get; set; }
+        public List<Choice> Choices { get; set; }
+        public Usage Usage { get; set; }
+    }  
+
+    public class Choice
+    {
+        public int Index { get; set; }
+        public Message Message { get; set; }
+        public string FinishReason { get; set; }
+    }
+
+    public class Message
+    {
+        public string Role { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class Usage
+    {
+        public int PromptTokens { get; set; }
+        public int CompletionTokens { get; set; }
+        public int TotalTokens { get; set; }
+    }
+}
---
+++ CodeWhispererAI/Services/OpenAIService.cs
@@ -0,0 +1,71 @@
+﻿using System.Net.Http.Headers;
+using System.Net.Http;
+using CodeWhispererAI.DataAccess;
+using CodeWhispererAI.Models;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System.Text;
+using Serilog;
+
+namespace CodeWhispererAI.Services
+{
+    public class OpenAIService
+    {
+        private static HttpClient _client;
+        private readonly IConfiguration _configuration;
+
+        public OpenAIService( IConfiguration configuration)
+        {
+            _configuration = configuration;
+            InitializeHttpClient();
+        }
+
+        private void InitializeHttpClient()
+        {
+            if (_client == null)
+            {
+                _client = new HttpClient()
+                {
+                    BaseAddress = new Uri("https://api.openai.com/")
+                };
+
+                _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _configuration["OpenAIAPIKey"]);
+                _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            }
+        }
+
+        public async Task<ChatCompletion> PostAndGetChatCompletion(string prompt)
+        {
+            var requestData = new
+            {
+                model = "gpt-3.5-turbo",
+                messages = new[]
+                {
+                    new { role = "system", content = "You are a code analyzer" },
+                    new { role = "user", content = prompt }
+                },
+                temperature = 0.5,
+                max_tokens = 60
+            };
+
+
+            string jsonRequestData = JsonConvert.SerializeObject(requestData);
+            var content = new StringContent(jsonRequestData, Encoding.UTF8, "application/json");
+
+            HttpResponseMessage response = await _client.PostAsync("v1/chat/completions", content);
+
+            if(response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                var chatCompletion = JsonConvert.DeserializeObject<ChatCompletion>(responseContent);
+
+                return chatCompletion;
+            }
+            else
+            {
+                Log.Error("GBT 3.5 Turbo API call failed");
+                throw new HttpRequestException($"Error: {response.StatusCode}");
+            }
+        }
+    }
+}
