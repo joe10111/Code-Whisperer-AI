edda79d05b05a5c59c14b410ddc96b2c316dd9be
Joe Centeno <centeno.joe@gmail.com>
2024-01-09 14:31:54 -0800

    Finished Account Refactor

--- CodeWhispererAI/Controllers/UserCodeDataController.cs
+++ CodeWhispererAI/Controllers/UserCodeDataController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Identity;
 using CodeWhispererAI.DataAccess;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace CodeWhispererAI.Controllers
 {
@@ -11,24 +12,62 @@ namespace CodeWhispererAI.Controllers
     {
         private readonly CodeWhispererAIContext _context;
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IMemoryCache _memoryCache;
 
-        public UserCodeDataController(CodeWhispererAIContext context, UserManager<ApplicationUser> userManager)
+
+        public UserCodeDataController(CodeWhispererAIContext context, UserManager<ApplicationUser> userManager, IMemoryCache memoryCache)
         {
             _context = context;
             _userManager = userManager;
+            _memoryCache = memoryCache;
         }
 
         // GET: UserCodeData
         public async Task<IActionResult> Index()
         {
-            var userId = _userManager.GetUserId(User);
-            var viewModel = new UserCodeDataViewModel
+            var userId = GetUserIdFromCookie(HttpContext);
+            if (userId == null)
             {
-                CodeSnippets = await _context.CodeSnippets.Where(c => c.ApplicationUserId == userId).ToListAsync(),
-                CodeAnalyses = await _context.CodeAnalyses.Where(c => c.ApplicationUserId == userId).ToListAsync()
-            };
+                // Handle the case where the user ID is not in the cookie
+                return View(new UserCodeDataViewModel()); // Or redirect, as appropriate
+            }
+
+            var viewModel = new UserCodeDataViewModel();
+
+            // Construct the cache keys
+            var snippetCacheKey = $"CodeSnippet_{userId}";
+            var analysisCacheKey = $"CodeAnalysis_{userId}";
+
+            // Try to retrieve the entire collection of CodeSnippets from cache
+            if (_memoryCache.TryGetValue(snippetCacheKey, out IEnumerable<CodeSnippet> cachedSnippets))
+            {
+                viewModel.CodeSnippets = cachedSnippets;
+            }
+            else
+            {
+                viewModel.CodeSnippets = Enumerable.Empty<CodeSnippet>();
+            }
+
+            // Try to retrieve the entire collection of CodeAnalyses from cache
+            if (_memoryCache.TryGetValue(analysisCacheKey, out IEnumerable<CodeAnalysis> cachedAnalyses))
+            {
+                viewModel.CodeAnalyses = cachedAnalyses;
+            }
+            else
+            {
+                viewModel.CodeAnalyses = Enumerable.Empty<CodeAnalysis>();
+            }
 
             return View(viewModel);
+        }
+
+        public string GetUserIdFromCookie(HttpContext context)
+        {
+            if (context.Request.Cookies.TryGetValue("UserIdentifier", out string userId))
+            {
+                return userId;
+            }
+            return null; // Or handle it as per your application's logic
         }
     }
 
